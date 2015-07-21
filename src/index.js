39bb2cd6f36a829209dca13cf05156695d5e63d1
anthonydelciotto <anthdel.developer@gmail.com>
2015-07-21 21:00:47 +1000

    enabled antialias and linear css scaling by default

--- src/index.js
+++ src/index.js
@@ -9,11 +9,12 @@
 var states = require('./states');
 
 window.onload = function() {
-    var game = new Phaser.Game(400, 240, Phaser.AUTO, 'game', null, false, false);
+    var game = new Phaser.Game(400, 240, Phaser.AUTO, 'game', null);
 
     // add all the game states and then start the boot state
     _.each(states, (val, key) => {
         game.state.add(`${key}`, val);
     });
+
     game.state.start('boot');
 };
