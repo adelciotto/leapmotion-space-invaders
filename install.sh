53ba8036656892bf2c8bbe82d45f74b91443b7ca
Anthony Del Ciotto <anthdel.developer@gmail.com>
2015-08-05 16:22:32 +1000

    updated install.sh

--- install.sh
+++ install.sh
@@ -11,14 +11,17 @@ if [[ -z "$1" || -z "$2" ]]; then
 fi
 
 # check if node and npm is installed
+echo "checking if node is installed..."
 if !hash node 2>/dev/null; then
   echo "install node bro..."
   exit 0
 fi
 
 ## set the new remote repo
+echo "setting new remote origin url"
 git remote set-url origin https://github.com/$1/$2.git
 
 ## install dependencies
 npm i
+echo ""
 echo "start developing: run npm watch"
